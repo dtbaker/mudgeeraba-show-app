ba174b185ed47a596908e80771dc7995d8890b2c
dtbaker <dtbaker@gmail.com>
2017-06-14 09:43:26 +1000

    splash screen and ios status bar fixes

--- js/my-app-init.js
+++ js/my-app-init.js
@@ -8,9 +8,6 @@ var myApp = new Framework7({
 // Export selectors engine
 var $$ = Dom7;
 
-
-
-
 // Add view
 var mainView = myApp.addView('.view-main', {
     // Because we use fixed-through navbar we can enable dynamic navbar
@@ -19,3 +16,6 @@ var mainView = myApp.addView('.view-main', {
 
 myApp.showPreloader('Loading...');
 
+if (!window.navigator.onLine) {
+    myApp.alert('Please enable mobile internet for this app to work correctly.');
+}
